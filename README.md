0d92b4d35ad7dbff54cb43e141763ffce9bccbe7
sethyuan <cavingdeep@gmail.com>
2013-05-03 17:22:43 +0800

    Implementation and tests.

---
+++ README.md
@@ -0,0 +1,67 @@
+# jsutils
+
+Utilities for JavaScript.
+
+## Installation
+
+```bash
+$ npm install jsutils
+```
+
+## Example
+
+```js
+var js = require("jsutils");
+
+// Extend
+var x = {
+  name: "x"
+};
+var y = {
+  name: "y",
+  val: 10
+};
+js.extend(x, y); // returns x
+
+// Clone
+var y = {
+  name: "y",
+  attrs: ["a", "b", {name: "c", pos: 3}],
+  modDate: new Date()
+};
+var x = js.clone(y);
+```
+
+## API
+
+### jsutils.extend(dest, src)
+
+Assign any `src` own, non-inherited properties to `dest`, overriding if necessary.
+
+### jsutils.clone(src)
+
+Deep clone `src`. Supports simple values, Object, Array, Date and RegExp.
+
+## License
+
+(The MIT License)
+
+Copyright (c) 2013 Seth Yuan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ test/jsutils.js
@@ -0,0 +1,114 @@
+var utils = require("../"),
+    should = require("should");
+
+describe("extend", function() {
+  it("simple object", function() {
+    var x = {name: "Seth"};
+    var y = {age: 30};
+    var z = utils.extend(x, y);
+    z.should.equal(x);
+    x.should.have.property("age").equal(y.age);
+  });
+
+  it("simple object override", function() {
+    var x = {name: "Seth"};
+    var y = {age: 30, name: "Fiona"};
+    utils.extend(x, y);
+    x.should.have.property("age").equal(y.age);
+    x.name.should.eql("Fiona");
+  });
+
+  it("src is no object nor function", function() {
+    var x = {name: "Seth"};
+    var y = 20;
+    (function() {
+      utils.extend(x, y);
+    }).should.throw();
+  });
+
+  it("dest is no object nor function", function() {
+    var x = 20;
+    var y = {name: "Seth"};
+    (function() {
+      utils.extend(x, y);
+    }).should.throw();
+  });
+
+  it("src is null", function() {
+    var x = {name: "Seth"};
+    var y = null;
+    (function() {
+      utils.extend(x, y);
+    }).should.throw();
+  });
+
+  it("dest is null", function() {
+    var x = null;
+    var y = {name: "Seth"};
+    (function() {
+      utils.extend(x, y);
+    }).should.throw();
+  });
+
+  it("src is function", function() {
+    var x = {name: "Seth"};
+    var y = function() {};
+    y.foo = "foo";
+    utils.extend(x, y).should.have.property("foo").equal(y.foo);
+  });
+
+  it("src is array", function() {
+    var x = {name: "Seth"};
+    var y = {toys: ["car", "xbox"] };
+    utils.extend(x, y);
+    x.should.have.property("toys").equal(y.toys);
+  });
+});
+
+describe("clone", function() {
+  it("simple object", function() {
+    var x = {name: "Seth"};
+    var y = utils.clone(x);
+    y.should.have.property("name").eql(x.name);
+  });
+
+  it("object with array", function() {
+    var x = {name: "Seth", toys: ["car", "xbox"]};
+    var y = utils.clone(x);
+    y.should.have.property("toys");
+    y.toys.should.not.equal(x.toys);
+    y.toys.should.eql(x.toys);
+  });
+
+  it("object with complex array", function() {
+    var x = {name: "Seth", toys: ["car", {kind: "game console", name: "xbox"}]};
+    var y = utils.clone(x);
+    y.should.have.property("toys");
+    y.toys.should.not.equal(x.toys);
+    y.toys.should.eql(x.toys);
+  });
+
+  it("object with object", function() {
+    var x = {name: "Seth", edu: {elementary: "xx"}};
+    var y = utils.clone(x);
+    y.should.have.property("edu");
+    y.edu.should.not.equal(x.edu);
+    y.edu.should.have.property("elementary").eql(x.edu.elementary);
+  });
+
+  it("object with Date", function() {
+    var x = {name: "Seth", birth: new Date(2000, 12, 1)};
+    var y = utils.clone(x);
+    y.should.have.property("birth");
+    y.birth.should.not.equal(x.birth);
+    y.birth.getTime().should.eql(x.birth.getTime());
+  });
+
+  it("object with RegExp", function() {
+    var x = {name: "Seth", crit: /abc/g};
+    var y = utils.clone(x);
+    y.should.have.property("crit");
+    y.crit.should.not.equal(x.crit);
+    y.crit.toString().should.eql(x.crit.toString());
+  });
+});
